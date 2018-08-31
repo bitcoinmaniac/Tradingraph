15aefa305d4f5111319acc367716124febfdf614
eiamei <maximambrosevich@gmail.com>
2018-08-31 11:47:04 +0300

    build test

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Cryptochart from './components/Cryptochart.vue';
-
-export default Cryptochart;
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('crypto-chart', Cryptochart);
-}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Cryptochart from './App.vue';
+
+// export default Cryptochart;
+//
+// if (typeof window !== 'undefined' && window.Vue) {
+//   window.Vue.component('crypto-chart', Cryptochart);
+// }
